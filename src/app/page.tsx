f39f51d90f7b5a8143ab04df201469dacd6cc543
mindful-sense <>
2025-09-21 22:51:00 +0400

    prep style the content column and add prettier-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main className="py-[9rem] mx-[0.5rem]"></main>;
+  return (
+    <main className="mx-auto min-h-screen max-w-[76.5rem] px-[0.5rem] py-[9rem]"></main>
+  );
 }
